ef2f449273fd2d1e4bf0813e9ea24863211ef62c
lukaslukas98 <lukaslukas98@gmail.com>
2023-06-22 16:48:56 +0300

    Barracks module rework
    -UI fixed
    -Calculations nearly fixed
    --Promotion cost incorrect
    --CountPerS not working on last tier

--- Assets/Scripts/BarracksController.cs
+++ Assets/Scripts/BarracksController.cs
@@ -15,43 +15,53 @@ public class BarracksController : MonoBehaviour
 
     static BarracksController controller1;
 
-    List<Unit> units = new List<Unit>();
+    public static List<Unit> units = new List<Unit>();
     RectTransform unitListRectTransform;
 
-    public class Soldier : Unit
-    {
-        public Soldier(UnitType type, int tier, int count, int incrementPerS, TextMeshProUGUI unitCountText, TextMeshProUGUI unitPerSText, Button promoteUnitButton) : base(type, tier, count, incrementPerS, unitCountText, unitPerSText, promoteUnitButton)
-        {
-        }
-    }
+    //public class Soldier : Unit
+    //{
+    //    public Soldier(UnitType type, int tier, int count, int incrementPerS, TextMeshProUGUI unitCountText, TextMeshProUGUI unitPerSText, Button promoteUnitButton) : base(type, tier, count, incrementPerS, unitCountText, unitPerSText, promoteUnitButton)
+    //    {
+    //    }
+    //}
 
     // Start is called before the first frame update
     void Start()
     {
         unitListRectTransform = unitListContainer.GetComponent<RectTransform>();
         controller1 = this;
-        GameObject unitField = Instantiate(unitFieldPrefab,new Vector2(unitListContainer.transform.position.x, unitListContainer.transform.position.y+40), unitListContainer.transform.rotation, unitListContainer.transform);
-        units.Add(new Unit(Unit.UnitType.Soldier,0,0,0, unitField.transform.GetChild(0).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(1).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(2).GetComponent<Button>()));
-        unitField.GetComponent<TextMeshProUGUI>().text = "T1 Soldiers:";
+        GameObject unitField = Instantiate(unitFieldPrefab,new Vector2(0, 0), unitListContainer.transform.rotation, unitListContainer.transform);
+        units.Add(new Unit(Unit.UnitType.Soldier,0,0,0, unitField.transform.GetChild(0).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(1).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(2).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(3).GetComponent<Button>()));
+        unitField.transform.localPosition = unitListRectTransform.localPosition;
+        units[0].unitText.text = "T1 Soldiers:";
         units[0].promoteUnitButton.onClick.RemoveAllListeners();
-        units[0].promoteUnitButton.onClick.AddListener(() => units[0].AddCount(1,1,1));
+        units[0].promoteUnitButton.onClick.AddListener(() => units[0].AddCount(1,1));
         units[0].promoteUnitButton.transform.GetComponentInChildren<TextMeshProUGUI>().text = "Train";
         StartCoroutine("Increment");
     }
 
     public void InstantiateNewTier(int tier)
     {
-        GameObject unitField = Instantiate(unitFieldPrefab, new Vector2(unitListContainer.transform.position.x, unitListContainer.transform.position.y-(30*tier)), unitListContainer.transform.rotation, unitListContainer.transform);
-        units.Add(new Unit(Unit.UnitType.Soldier, tier, 0, 0, unitField.transform.GetChild(0).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(1).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(2).GetComponent<Button>()));
-
-        unitListRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 120 + ((tier) * 70));
-        unitField.GetComponent<TextMeshProUGUI>().text = "T"+(tier+1)+" Soldiers:";
+        GameObject unitField = Instantiate(unitFieldPrefab, new Vector2(0, 0), unitListContainer.transform.rotation, unitListContainer.transform);
+        units.Add(new Unit(Unit.UnitType.Soldier, tier, 0, 0, unitField.transform.GetChild(0).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(1).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(2).GetComponent<TextMeshProUGUI>(), unitField.transform.GetChild(3).GetComponent<Button>()));
+        unitField.transform.localPosition = new Vector2(unitListRectTransform.localPosition.x, unitListRectTransform.localPosition.x - (120 * (tier))+30);
+        unitField.transform.GetChild(3).GetComponent<Button>().onClick.AddListener(() => PromoteSoldiers(tier));
+        //unitListRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical)
+        //unitListRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 120+(70 * tier));
+        //Resize(70, new Vector3(0, 1, 0), unitListRectTransform);
+        unitField.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "T"+(tier+1)+" Soldiers:";
     }
 
     public void PromoteSoldiers(int index)
     {
-        units[index - 1].AddCount(-PromotionCost(index), units[index].count,units.Count);
-        units[index].AddCount(1, units[index+1].count, units.Count);
+        units[index - 1].AddCount(-PromotionCost(index), units.Count);
+        units[index].AddCount(1, units.Count);
+        Debug.Log(index + " total count" + units.Count);
+        if(units.Count > index+1)
+        {
+            Debug.Log("checked");
+            units[index + 1].CheckPromoteButton(index);
+        }
     }
 
     IEnumerator Increment()
@@ -62,6 +72,7 @@ public class BarracksController : MonoBehaviour
             {
                 units[i].AddCountPerS(units[i+1].count);
                 units[i].RecalculateIncrement(units[i + 1].count);
+                units[i+1].CheckPromoteButton(i-1);
 
             }
             CheckForNextTier(units[units.Count-1]);
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -20,15 +20,17 @@ public class Unit : MonoBehaviour
     public int tier;
     public int count;
     int incrementPerS;
+    public TextMeshProUGUI unitText;
     TextMeshProUGUI unitCountText;
     TextMeshProUGUI unitPerSText;
     public Button promoteUnitButton;
 
-    public Unit(UnitType type, int tier, int count, int incrementPerS, TextMeshProUGUI unitCountText, TextMeshProUGUI unitPerSText, Button promoteUnitButton)
+    public Unit(UnitType type, int tier, int count, int incrementPerS, TextMeshProUGUI unitText, TextMeshProUGUI unitCountText, TextMeshProUGUI unitPerSText, Button promoteUnitButton)
     {
         this.type = type;
         this.tier = tier;
         this.count = count;
+        this.unitText = unitText;
         this.incrementPerS = incrementPerS;
         this.unitCountText = unitCountText;
         this.unitPerSText = unitPerSText;
@@ -50,38 +52,39 @@ public class Unit : MonoBehaviour
         if (tier != 0)
             CheckPromoteButton(tier);
     }
-    public void AddCount(int amount, int higherTierCount, int lastTierUnlocked)
+    public void AddCount(int amount, int lastTierUnlocked)
     {
-        count += amount;
+        count += amount+100;
         UpdateCount();
-        if (tier < lastTierUnlocked - 1)
+        if ( tier > 1)
         {
-            RecalculateIncrement(higherTierCount);
-            CheckPromoteButton(tier + 1);
-        }
-        if (tier != 0)
+          //  RecalculateIncrement(higherTierCount);
             CheckPromoteButton(tier);
+        }
+     //   if (tier > 1)
+         //   CheckPromoteButton(tier-1);
     }
-        private void UpdateCount()
-        {
-            unitCountText.text = count.ToString();
-        }
-
-        public void UpdatePromoteButton(bool state)
-        {
-            promoteUnitButton.interactable = state;
-        }
-
-        public void RecalculateIncrement(int higherTierCount)
-        {
-            incrementPerS = higherTierCount / 10;// (tier*2 + 1)/3;
-            unitPerSText.text = "+" + incrementPerS.ToString() + "/s";
-        }
-
-
-    private void CheckPromoteButton(int previousTierCount)
+    private void UpdateCount()
     {
-        if (promotionCost - previousTierCount <= 0)
+        unitCountText.text = count.ToString();
+    }
+
+    public void UpdatePromoteButton(bool state)
+    {
+        promoteUnitButton.interactable = state;
+    }
+
+    public void RecalculateIncrement(int higherTierCount)
+    {
+        incrementPerS = higherTierCount / 10;// (tier*2 + 1)/3;
+        unitPerSText.text = "+" + incrementPerS.ToString() + "/s";
+    }
+
+
+    public void CheckPromoteButton(int previousTier)
+    {
+       // Debug.Log("Previous tier index:"+ previousTier+" Count:"+ BarracksController.units[previousTier].count);
+        if (promotionCost - BarracksController.units[tier-1].count <= 0)
         {
             UpdatePromoteButton(true);
         }
